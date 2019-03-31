002fff2c9ebbf2b782700bd138973b9da1963e72
Robin Meis <robin.meis@smartnoob.de>
2019-03-31 15:21:32 +0200

    Introducing Callbacks

--- demo.py
+++ demo.py
@@ -1,6 +1,20 @@
 from MITMsmtp import MITMsmtp
 
+#Callback to be fired as soon as we receive login credentials
+def login(message, username, password):
+    print("Got username/password:")
+    print("Username" + username)
+    print("Password" + password)
+
+#Callback to be fired as soon as a message has been received completely
+def message(message):
+    print("Message complete!")
+
 SMTPServer = MITMsmtp("10.2.10.126", 8888)
-SMTPServer.start()
+messageHandler = SMTPServer.getMessageHandler() #Get Message Handler
+messageHandler.registerLoginCallback(login) #Register callback for login
+messageHandler.registerMessageCallback(message) #Register callback for complete messages
+
+SMTPServer.start() #Start SMTPServer
 input("Press enter to stop!\n")
-SMTPServer.stop()
+SMTPServer.stop() #Stop SMTPServer
