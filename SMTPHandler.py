20adb731e91922891c855fd05ddb3947ca69915a
Robin Meis <robin.meis@smartnoob.de>
2019-03-31 04:04:03 +0200

    First full SMTP Handler

--- SMTPHandler.py
+++ SMTPHandler.py
@@ -1,58 +1,107 @@
 from socketserver import StreamRequestHandler
+import re
+import base64
 
 class SMTPHandler(StreamRequestHandler):
+    def init(self):
+        self.client_name = None
+        self.username = None
+        self.password = None
+        self.sender = None
+        self.recipients = []
+        self.message = None
+
     def handle(self):
+        self.init()
         try:
-            #print('connection from', client_address)
-            self.connection.sendall(b'220 smtp.server.com Simple Mail Transfer Service Ready\r\n')
-            # Receive the data in small chunks and retransmit it
+            self.sendGreeting()
+            self.readEHLO()
+            self.sendHELLO()
+            self.sendAuthTypes()
+            self.readAuth()
+            self.acceptAuth()
+            self.readSender()
+            self.sendOK()
+            self.readRecipients()
+            self.sendIntermediate()
+            self.readMSG()
+            self.sendOK()
 
-            message = ""
-            while True:
-                bit = self.connection.recv(1)
-                if (len(message) > 0 and (bit == b'\n' or bit == b'\r')):
-                    break
-                elif (bit != b''):
-                    message += bit.decode("ASCII")
-
-            print(message)
-
-            self.connection.sendall(b'250-smtp.server.com Hello client.example.com\r\n')
-            self.connection.sendall(b'250-SIZE 1000000\r\n')
-            self.connection.sendall(b'250 AUTH PLAIN\r\n')
-
-            message = ""
-            while True:
-                bit = self.connection.recv(1)
-                if (len(message) > 0 and (bit == b'\n' or bit == b'\r')):
-                    break
-                elif (bit != b''):
-                    message += bit.decode("ASCII")
-
-            print(message)
-
-            self.connection.sendall(b'334\r\n')
-
-            message = ""
-            while True:
-                bit = self.connection.recv(1)
-                if (len(message) > 0 and (bit == b'\n' or bit == b'\r')):
-                    break
-                elif (bit != b''):
-                    message += bit.decode("ASCII")
-
-            print(message)
-
-            self.connection.sendall(b'235 2.7.0 Authentication successful\r\n')
-
-            while True:
-                bit = self.connection.recv(1)
-                if (len(message) > 0 and (bit == b'\n' or bit == b'\r')):
-                    break
-                elif (bit != b''):
-                    message += bit.decode("ASCII")
-
-            print(message)
+        except Exception as e:
+            print(e)
+            print("Closed connection!")
+            self.connection.close()
         finally:
             # Clean up the connection
             self.connection.close()
+
+    def sendGreeting(self):
+        self.connection.sendall(b'220 smtp.server.com Simple Mail Transfer Service Ready\r\n')
+
+    def readEHLO(self):
+        line = self.rfile.readline().strip().decode("ASCII")
+        match = re.match("EHLO (.*)", line)
+        if (match == None):
+            raise ValueError("Invalid EHLO sent by client")
+        self.client_name = match.group(1)
+
+    def sendHELLO(self):
+        self.connection.sendall(b'250-smtp.server.com Hello ' + self.client_name.encode("ASCII") + b'\r\n')
+        self.connection.sendall(b'250-SIZE 1000000\r\n')
+
+    def sendAuthTypes(self):
+        self.connection.sendall(b'250 AUTH PLAIN\r\n')
+
+    def readAuth(self):
+        line = self.rfile.readline().strip().decode("ASCII")
+        match = re.match("AUTH PLAIN ([A-Za-z0-9]*=*)$", line)
+        if (match == None):
+            raise ValueError("Unsupported Authentication Type requested by client")
+
+        auth = base64.b64decode(match.group(1)).decode("ASCII").split('\x00')
+
+        if (len(auth) < 2):
+            raise ValueError("Username/Password not found")
+
+        self.username = auth[-2]
+        self.password = auth[-1]
+
+        print("Username: " + self.username)
+        print("Password: " + self.password)
+
+    def acceptAuth(self):
+        self.connection.sendall(b'235 2.7.0 Authentication successful\r\n')
+
+    def readSender(self):
+        line = self.rfile.readline().strip().decode("ASCII")
+        match = re.match("MAIL FROM:\<([a-zA-z0-9]*@[a-zA-z0-9\.]*)\>", line)
+        if (match == None):
+            raise ValueError("Could not read sender")
+
+        self.sender = match.group(1)
+
+    def sendOK(self):
+        self.connection.sendall(b'250 OK\r\n')
+
+    def readRecipients(self):
+        while True:
+            line = self.rfile.readline().strip().decode("ASCII")
+            if (line == "DATA"):
+                return
+
+            match = re.match("RCPT TO:\<([a-zA-z0-9]*@[a-zA-z0-9\.]*)\>", line)
+            if (match == None):
+                raise ValueError("Could not read recipients")
+            self.sendOK()
+
+    def sendIntermediate(self):
+        self.connection.sendall(b'354 Intermediate\r\n')
+
+    def readMSG(self):
+        self.message = ""
+        while True:
+            line = self.rfile.readline().strip().decode("ASCII")
+            if (line == '.'):
+                print(self.message)
+                return
+            self.message += line + "\r\n"
