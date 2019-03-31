56e09edcc1a7aad6b4e818aab37f186acfc24078
Robin Meis <robin.meis@smartnoob.de>
2019-03-31 03:57:28 +0200

    Added line break

--- demo.py
+++ demo.py
@@ -2,5 +2,5 @@ from MITMsmtp import MITMsmtp
 
 SMTPServer = MITMsmtp("10.2.10.126", 8888)
 SMTPServer.start()
-input("Press enter to stop!")
+input("Press enter to stop!\n")
 SMTPServer.stop()
--- demo_ssl.py
+++ demo_ssl.py
@@ -2,5 +2,5 @@ from MITMsmtp import MITMsmtp
 
 SMTPServer = MITMsmtp("10.2.10.126", 8888, True, "certs/MITMsmtp.crt", "certs/MITMsmtp.key")
 SMTPServer.start()
-input("Press enter to stop!")
+input("Press enter to stop!\n")
 SMTPServer.stop()
