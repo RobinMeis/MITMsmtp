c25a947a599a8ce16c56277ba1f311e0d7c025da
Robin Meis <robin.meis@smartnoob.de>
2019-04-03 01:13:09 +0200

    Added alternative mode for AUTH PLAIN

--- MITMsmtp/auth/authPlain.py
+++ MITMsmtp/auth/authPlain.py
@@ -5,10 +5,30 @@ import base64
 class authPlain(authMethod):
     def __init__(self, SMTPHandler, authLine):
         self.SMTPHandler = SMTPHandler
+        self.authLine = authLine
         self.username = None
         self.password = None
 
-        match = re.match("AUTH PLAIN ([A-Za-z0-9]*=*)$", authLine)
+        match = re.match("AUTH PLAIN$", authLine)
+        if (match != None):
+            self.auth()
+        else:
+            self.fastAuth()
+
+    @staticmethod
+    def toString():
+        return "PLAIN"
+
+    @staticmethod
+    def matchMethod(authLine):
+        match = re.match("AUTH PLAIN", authLine)
+        if (match == None):
+            return False
+        else:
+            return True
+
+    def fastAuth(self):
+        match = re.match("AUTH PLAIN ([A-Za-z0-9]*=*)$", self.authLine)
         if (match == None):
             raise ValueError("Failed to perform AUTH PLAIN")
 
@@ -17,19 +37,21 @@ class authPlain(authMethod):
         if (len(auth) < 2):
             raise ValueError("Username/Password not found")
 
-        self.SMTPHandler.writeLine("235 2.7.0 Authentication successful")
-
+        self.authSuccess()
         self.username = auth[-2]
         self.password = auth[-1]
 
-    @staticmethod
-    def toString():
-        return "PLAIN"
+    def auth(self):
+        self.SMTPHandler.writeLine("344")
+        line = self.SMTPHandler.readLine() #Read base64 encoded credentials
+        auth = base64.b64decode(line).decode("ASCII").split('\x00')
+        if (len(auth) < 2):
+            raise ValueError("Username/Password not found")
 
-    @staticmethod
-    def matchMethod(authLine):
-        match = re.match("AUTH PLAIN ([A-Za-z0-9]*=*)$", authLine)
-        if (match == None):
-            return False
-        else:
-            return True
+        self.authSuccess()
+        self.username = auth[-2]
+        self.password = auth[-1]
+        authSuccess()
+
+    def authSuccess(self):
+        self.SMTPHandler.writeLine("235 2.7.0 Authentication successful")
