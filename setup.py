875688da4df2c26407d3749427525232378b9b9a
Robin Meis <robin.meis@smartnoob.de>
2019-04-03 14:22:01 +0200

    Dev Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MITMsmtp",
-    version="0.0.1",
+    version="0.0.2-dev",
     author="Robin Meis",
     author_email="blog@smartnoob.de",
     description="An evil SMTP Server for client pentesting",
